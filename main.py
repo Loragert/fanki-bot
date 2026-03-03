92c25988bfe5c4d062c27382dafd5ef751e2ce3b
Loragert <uneas10121990@gmail.com>
2026-03-03 11:26:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1144,25 +1144,25 @@ async def handle_user_message(update: Update, context: ContextTypes.DEFAULT_TYPE
           #  ]
         #])
 
-        await context.bot.send_photo(
-            pass
-            ADMIN_ID[0],
-            file_id,
-            caption=f"ID: {user_id}\nTask: {task['task_id']}",
-            reply_markup=keyboard
-        )
+        #await context.bot.send_photo(
+            #pass
+            #ADMIN_ID[0],
+           # file_id,
+          #  caption=f"ID: {user_id}\nTask: {task['task_id']}",
+           # reply_markup=keyboard
+        #)
 
-        await update.message.reply_text(
-            pass
-            "Скрін відправлено на перевірку."
-        )
-        current_task.pop(user_id, None)
+       # await update.message.reply_text(
+           # pass
+            #"Скрін відправлено на перевірку."
+       # )
+      #  current_task.pop(user_id, None)
 
-        if task["social"] == "Google Maps":
-            pass
-            user_state[user_id] = None
-            await show_main_menu(update)
-            return
+       # if task["social"] == "Google Maps":
+           # pass
+          #  user_state[user_id] = None
+           # await show_main_menu(update)
+          #  return
 
         user_state[user_id] = None
         await show_main_menu(update)
@@ -1505,6 +1505,7 @@ if __name__ == "__main__":
     print("FankiBot Production Ready 🚀")
 
     app.run_polling(drop_pending_updates=True)
+
 
 
 
