842b626b17a9ac5ece4773db8a935f920644be0e
Loragert <uneas10121990@gmail.com>
2026-03-04 21:57:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -663,7 +663,7 @@ async def send_next_task(update: Update, user_id: str):
         if not t.get("active"):
             continue
 
-        if str(t.get("social_network")).lower() !=str(social_network).lower()
+        if str(t.get("social_network")).lower() !=str("social_network").lower()
             continue
         tid = t.get("task_id")
         if tid not in unique_templates:
@@ -1575,6 +1575,7 @@ if __name__ == "__main__":
     print("FankiBot Supabase Version 🚀")
 
     app.run_polling(drop_pending_updates=True)
+
 
 
 
